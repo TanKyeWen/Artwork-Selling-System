3b74f3095ec287cec30430229318c8f8e2ca4566
TanKyeWen <122182791+TanKyeWen@users.noreply.github.com>
2024-07-25 12:20:59 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package Assignment3;
 
 import java.io.IOException;
 import java.util.*;
-public class TestTest {
+public class Main {
 	public static void main(String[] args){
 		UXGalleryInterface interfaces = new UXGalleryInterface();
 	}
